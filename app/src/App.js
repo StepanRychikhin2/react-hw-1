fd44938d8e2024e3d9c401fd26ebde8b67e285f0
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2024-07-02 22:59:28 +0300

    фцвфцвцввфв

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Greeting , Message } from "./greating";
-import  Button  from "./btn";
-import './App.css';
+import React from "react";
+import { Greeting, Message } from "./greating";
+import Button from "./btn";
+import "./App.css";
 
 function App() {
   const btnClick = () => {
-    console.log("awdawdadad")
-  }
+    console.log("awdawdadad");
+  };
   return (
     <div className="App">
-     <Greeting className="re" name="Stepan" />
-     <Message text ="wad'wdadd" />
-     <Button  className="btn" onClick={btnClick}/>
+      <Greeting className="re" name="Stepan" />
+      <Message text="wad'wdadd" />
+      <Button className="btn" onClick={btnClick} />
     </div>
   );
 }
--- app/src/greating.js
+++ app/src/greating.js
@@ -1,13 +1,10 @@
 import React from "react";
 function Greeting({ name }) {
-    return <h1>Ghbdsn, {name}</h1>;
+  return <h1>Ghbdsn, {name}</h1>;
 }
 export default Greeting;
 
-function Message({text }) {
-    return <p>{text }</p>
+function Message({ text }) {
+  return <p>{text}</p>;
 }
-export {Greeting , Message}
-
-
-
+export { Greeting, Message };
